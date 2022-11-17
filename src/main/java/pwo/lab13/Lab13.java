e5bf6fc1667ecad1dcd8d92872dfe29bb7586d58
666666bg <65243723+666666bg@users.noreply.github.com>
2022-11-17 23:30:22 +0100

    lab13

---
+++ src/main/java/pwo/lab13/Lab13.java
@@ -0,0 +1,12 @@
+package pwo.lab13;
+
+/**
+ *
+ * @author Wladyslaw
+ */
+public class Lab13 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
